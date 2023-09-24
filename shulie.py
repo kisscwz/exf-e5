3c85a8a69f0d8e46da4ec20ac93c49520d207b9f
kisscwz <65785026+kisscwz@users.noreply.github.com>
2023-09-25 01:34:20 +0800

    Update shulie.py

--- shulie.py
+++ shulie.py
@@ -1,15 +1,10 @@
 
-# Python 斐波那契数列实现
- 
-# 获取用户输入数据
 nterms = int(input("你需要几项？"))
  
-# 第一和第二项
 n1 = 0
 n2 = 1
 count = 2
  
-# 判断输入的值是否合法
 if nterms <= 0:
    print("请输入一个正整数。")
 elif nterms == 1:
@@ -21,7 +16,6 @@ else:
    while count < nterms:
        nth = n1 + n2
        print(nth,end=" , ")
-       # 更新值
        n1 = n2
        n2 = nth
        count += 1
