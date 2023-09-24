a803f7ea8c1df325b97883d8adad4366d5b622e3
kisscwz <65785026+kisscwz@users.noreply.github.com>
2023-09-25 01:31:28 +0800

    Update wytp.py

--- wytp.py
+++ wytp.py
@@ -5,24 +5,19 @@ from bs4 import BeautifulSoup
 import os
 
 def download_images(url, save_dir):
-    # 发送请求获取网页内容
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # 创建保存图片的文件夹
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     
-    # 查找所有的图片标签
     img_tags = soup.find_all('img')
     
-    # 遍历图片标签，下载图片
     for img_tag in img_tags:
         img_url = img_tag['src']
         img_name = img_url.split('/')[-1]
         save_path = os.path.join(save_dir, img_name)
         
-        # 下载图片
         try:
             img_data = requests.get(img_url).content
             with open(save_path, 'wb') as img_file:
